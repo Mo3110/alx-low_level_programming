51dce48ce355393c0e0280e20ed0727a15715e12
Mo3110 <niche.hunter.31@gmail.com>
2023-03-16 06:31:39 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints a line to the standard error
+ *
+ * Retrun Always 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
