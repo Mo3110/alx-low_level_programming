410b660d7b1c7c97f93d9ce96dad339cd66e2e74
Mo3110 <niche.hunter.31@gmail.com>
2023-03-21 03:04:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description - prints the alphabet, in lowercase, followed by a new line
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	char 'a';
+
+	while (chr <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
