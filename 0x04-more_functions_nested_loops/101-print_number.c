da6627ebd0cdaccfd34aa73d5e91b44a0b373d20
Mo3110 <niche.hunter.31@gmail.com>
2023-03-23 07:55:38 -0700

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,21 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_number - Prints an integer.
- * @n: The integer to print.
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
  */
 void print_number(int n)
 {
-    int divisor = 1;
+	unsigned int i = n;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-
-    while (n / divisor >= 10)
-    {
-        divisor *= 10;
-    }
-
-    while (divisor != 0)
-    {
-        int digit = n / divisor;
-        _putchar('0' + digit);
-        n %= divisor;
-        divisor /= 10;
-    }
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
