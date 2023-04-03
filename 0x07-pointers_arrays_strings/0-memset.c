092f56c1b9b598a266b3e02596c0bce0a2db0565
Mo3110 <niche.hunter.31@gmail.com>
2023-04-03 13:54:26 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,12 +13,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < 0)
+	for (i = 0; i < n; i++)
 	{
 	s[i] = b;
-	i++;
 	}
 	return (s);
 }
