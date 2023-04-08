31d7b32676d39ca1d6db23399ad0921a5d166fca
Mo3110 <niche.hunter.31@gmail.com>
2023-04-08 05:38:49 -0700

    i

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
 
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, msg, sizeof(msg) - 1);
+    return 1;
 }
